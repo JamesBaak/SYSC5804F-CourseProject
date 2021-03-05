9180e0ab2ea2d90e3aebea5db786b4ea997d1505
Ben Earle <ben.earle@live.ca>
2021-03-05 13:30:57 -0500

    Channel Estimatio n is working well! :)^C

--- SimpleULA.m
+++ SimpleULA.m
@@ -12,7 +12,7 @@ c = physconst('lightspeed'); % speed of light in m/s
 fftPoints = 2048;
 df = 75 * 10^3; %hz
 BW = 90 * 10^6; %hz
-fc =3.5 * 10^9; %hz
+fc = 3.5 * 10^9; %hz
 pt = -20;% dbm (transmit power)
 lambda = c / fc; % carrier wavelength in m
 d = lambda / 2; % distance between antennas in m
@@ -34,24 +34,27 @@ cdl.CarrierFrequency = fc;
 cdl.MaximumDopplerShift = 0;
 
 cdl.TransmitAntennaArray.Size = [1 1 1 1 1];
-cdl.ReceiveAntennaArray.Size = [64 1 1 1 1];
+cdl.ReceiveAntennaArray.Size = [1 8 1 1 1];
 
 SR = 15.36e6;
 T = SR * 1e-3;
 cdl.SampleRate = SR;
 cdlinfo = info(cdl);
 Nt = cdlinfo.NumTransmitAntennas;
+Nr = cdlinfo.NumReceiveAntennas;
  
 txWaveform = ones(52,Nt);
 
 chInfo = info(cdl);
 
-rxWaveform = cdl(txWaveform);
+[rxWaveform, pathGains] = cdl(txWaveform);
+
+[rxWaveform, pathGains] = cdl(txWaveform);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Channel Reconstruction
 N = 52; % Sub-carrier count
-M = 64; % Antenna count
+M = Nr; % Antenna count
 
 % Get the set of path components from the channel model.
 L = length(chInfo.PathDelays);
@@ -59,21 +62,22 @@ tau = chInfo.PathDelays;
 % I do not know what anlge I should be using... hmmm
 % Likely CDL channel definition with antennas along x axis means I should
 % use a specific one, but which one? IDK yet
-theta = chInfo.AnglesZoA;
-gul = chInfo.AveragePathGains;
+theta = wrapTo180(chInfo.AnglesAoA - 180 .* ones(size(chInfo.AnglesAoA)));
+gul = squeeze(pathGains(1,:,1,:));
 
 % Reconstruct the channel using method from NOMP paper
 
 
 % Temporarily initialize the h to the exact size I am getting from kron
-h = zeros(64,52);
+h = zeros(N, M);
 for i = 1 : length(theta)
-    h = h + gul(i) .* kron(a(theta(i), M, d, lambda), p(tau(i), N, df)');
+     h = h + gul(i,:) .* kron(p(tau(i), N, df), a(theta(i), M, d, lambda)');
+%    h = h + gul(i) .* a(theta(i), M, d, lambda) * p(tau(i), N, df)';
 end
 
 disp("Error: ");
-mean(mean(h'-rxWaveform))
-
+mean(mean(h-rxWaveform))
+k = kron(p(tau(i), N, df), a(theta(i), M, d, lambda));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Function defs
