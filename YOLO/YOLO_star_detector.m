a31a53eccf662a8a37fa895b297924e3cc171536
James Baak <jamesbaak@protonmail.com>
2021-03-27 10:40:54 -0400

    YOLO improvements

--- YOLO/YOLO_star_detector.m
+++ YOLO/YOLO_star_detector.m
@@ -1,11 +1,11 @@
 % Load the labeled data
-data = load('spots.mat').gTruth;
+data = load('spots2.mat').gTruth;
 
 % Generate the appropriate path names
 [filepath,name,ext] = fileparts(string(data.DataSource.Source));
 imageFileNames = strings(length(name),1);
 for k = 1:length(name)
-   imageFileNames(k) = fullfile(pwd,'spots_images',append(name(k),ext(k)));
+   imageFileNames(k) = fullfile(pwd,'spots_images_2',append(name(k),ext(k)));
 end
 
 % Create structured table
@@ -20,6 +20,7 @@ trainingDataTbl = dataset(shuffledIndices(1:idx), :);
 testDataTbl = dataset(shuffledIndices(idx+1:end), :);
 
 % Image Datastore
+networkInputSize = [256 256 3];
 imdsTrain = imageDatastore(trainingDataTbl.imageFileNames);
 imdsTest = imageDatastore(testDataTbl.imageFileNames);
 
@@ -46,42 +47,32 @@ augmentedTrainingData = transform(trainingData, @augmentData);
 % montage(augmentedData, 'BorderSize', 10)
 
 % Training Setup ==========
-networkInputSize = [227 227 3];
 
 % Preprocessing testing ===
-% preprocessedTrainingData = transform(augmentedTrainingData, @(data)preprocessData(data, networkInputSize));
-% data = read(preprocessedTrainingData);
-% I = data{1,1};
-% bbox = data{1,2};
-% annotatedImage = insertShape(I, 'Rectangle', bbox);
-% annotatedImage = imresize(annotatedImage,2);
-% figure
-% imshow(annotatedImage)
-% reset(preprocessedTrainingData);
+preprocessedTrainingData = transform(augmentedTrainingData, @(data)preprocessData(data, networkInputSize));
+preprocessedValidationData = transform(testData,@(data)preprocessData(data,networkInputSize));
 
 rng(0)
-trainingDataForEstimation = transform(trainingData, @(data)preprocessData(data, networkInputSize));
+trainingDataForEstimation = transform(preprocessedTrainingData, @(data)preprocessData(data, networkInputSize));
 numAnchors = 6;
-[anchors, meanIoU] = estimateAnchorBoxes(trainingDataForEstimation, numAnchors);
+[anchorBoxes, meanIoU] = estimateAnchorBoxes(trainingDataForEstimation, numAnchors);
 
-area = anchors(:, 1).*anchors(:, 2);
-[~, idx] = sort(area, 'descend');
-anchors = anchors(idx, :);
-anchorBoxes = {anchors(1:3,:)
-    anchors(4:6,:)
-    };
 % Create network
-net = darknet53('Weights','None'); % Use darknet-53
-classNames = trainingDataTbl.Properties.VariableNames(2:end);
-lgraph = load('darknet_yolo.mat').lgraph_2;
+featureExtractionNetwork = darknet53; % Use darknet-53
+featureLayer = 'conv52';
+% featureExtractionNetwork = resnet50;
+% featureLayer = 'activation_40_relu';
 
-options = trainingOptions('sgdm',...
-          'InitialLearnRate',0.001,...
-          'Verbose',true,...
-          'MiniBatchSize',16,...
-          'MaxEpochs',30,...
-          'Shuffle','never',...
-          'VerboseFrequency',30,...
-          'CheckpointPath',tempdir);
+classNames = trainingDataTbl.Properties.VariableNames(2:end);
+numClasses = 1;
+
+lgraph = yolov2Layers(networkInputSize,numClasses,anchorBoxes,featureExtractionNetwork,featureLayer);
+
+options = trainingOptions('sgdm', ...
+        'MiniBatchSize',8, ....
+        'InitialLearnRate',1e-3, ...
+        'MaxEpochs',20, ... 
+        'CheckpointPath',tempdir, ...
+        'ValidationData',preprocessedValidationData);
       
-[detector,info] = trainYOLOv2ObjectDetector(trainingData,lgraph,options);
+[detector,info] = trainYOLOv2ObjectDetector(preprocessedTrainingData,lgraph,options);
